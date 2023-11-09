459265ae2af706db332ef05f98e5d246ec39c2e5
Rafael Leal <rafael.leal5@aluno.senai.br>
2023-11-09 15:00:08 -0300

    third commit

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import  express  from "express";
+import {config} from "dotenv";
+
+config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.status(200).json({message: "Hello, Word!"})
+});
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`)
+});
